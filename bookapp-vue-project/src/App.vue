94b025678dc5ed6df985109b7a56a69f44cdc824
kotomi.satou <piitun3232@gmail.com>
2021-09-05 20:00:00 +0900

    appページcss scopedテスト

--- bookapp-vue-project/src/App.vue
+++ bookapp-vue-project/src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>こんにちは、Vue.js!</p>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+const APP_TITLE = 'Vue.jsアプリ'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
+
+export function getTriagngle(base, height) {
+  return base * height / 2;
+}
+
+export class Article {
+  getAppTitle(){
+    return APP_TITLE;
+  }
+} 
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+p{
+  border: 1px solid Red;
+  background-color: Yellow;
 }
 </style>
